d49be0d89ca78639e0fb45890df0cd477883a928
andres <andresfelix951@gmail.com>
2023-09-22 16:16:43 -0500

    adding edit and delete

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index(){
+        return view('posts.index',[
+            'posts' => Post::latest()->paginate()
+        ]);
+    }
+
+    public function create(Post $post){
+        return view('posts.create', ['post' => $post]);
+    }
+
+    public function edit(Post $post){
+        return view('posts.edit', ['post' => $post]);    
+    }
+    
+    public function destroy(Post $post){
+        $post->delete();
+        return back();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(PageController::class)->group(function(){
@@ -21,5 +22,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('posts', PostController::class)->except('show');
 
 require __DIR__.'/auth.php';
